b0b30a7e2e46bc36ca3694ac099826e4b1c12393
andsala <5263089+andsala@users.noreply.github.com>
2018-04-13 16:05:39 +0200

    Add unsee-dl

---
+++ README.md
@@ -0,0 +1,9 @@
+# unsee-dl - Unsee downloader
+
+# Usage
+
+```sh
+pip install -r requirements.txt
+
+python unsee-dl.py [-o OUT_DIR] <id...>
+```
---
+++ unsee-dl.py
@@ -0,0 +1,70 @@
+import argparse
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def get_web_driver():
+    opts = Options()
+    opts.set_headless(True)
+    opts.set_preference("network.cookie.alwaysAcceptSessionCookies", True)
+    opts.set_preference("network.cookie.cookieBehavior", 1)
+
+    browser = webdriver.Firefox(firefox_options=opts, log_path='/tmp/geckodriver.log')
+    browser.implicitly_wait(10)
+    return browser
+
+
+def unsee_download(driver, image_id, out_path='.'):
+    driver.get("https://unsee.cc/{:s}/".format(image_id))
+
+    canvas = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//canvas[@class='image']"))
+    )
+
+    # Remove overlay elements
+    driver.execute_script('''
+        var chat = document.getElementById("chat");
+        chat.parentNode.removeChild(chat);
+        
+        var report = document.getElementsByClassName("report");
+        for (index = report.length - 1; index >= 0; index--) {
+            report[index].parentNode.removeChild(report[index]);
+        }
+        
+        var grid = document.getElementsByClassName("toggle_grid");
+        for (index = grid.length - 1; index >= 0; index--) {
+            grid[index].parentNode.removeChild(grid[index]);
+        }
+    ''')
+
+    canvas.screenshot("{}/{}.png".format(out_path, image_id))
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="unsee.cc downloader")
+    parser.add_argument('-o', '--out', action="store", dest="out_dir", type=str, default=".",
+                        help="Output directory")
+    parser.add_argument('ids', action="store", nargs='+',
+                        help="unsee.cc IDs to download")
+    args = parser.parse_args(argv[1:])
+
+    driver = get_web_driver()
+
+    for image_id in args.ids:
+        # noinspection PyBroadException
+        try:
+            print("Downloading {:s}... ".format(image_id), end='')
+            unsee_download(driver, image_id, args.out_dir)
+            print("done")
+        except Exception:
+            print("fail")
+    driver.quit()
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
